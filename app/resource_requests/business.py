51ebe980742a1060a8e38565e612f832e180fedc
Ravi Chamakuri <rkc7h@virginia.edu>
2024-11-15 08:24:19 -0700

    Testing cors

--- app/resource_requests/business.py
+++ app/resource_requests/business.py
@@ -8,7 +8,7 @@ class UVARCResourcRequestFormInfoDataManager():
 
     def get_resource_request_from_info(self, request):
         return {
-            'request_base_url': request.host_url,
+            'request_base_url': request.headers.get('Origin'),
             'is_user_resource_request_elligible':  self.__uvarc_user_data_manager .is_user_resource_request_elligible(),
             'user_groups': self.__uvarc_user_data_manager.get_user_groups_info()
         }
