cd8539082b18d017f2f7b96f9c0743b33b3a6a48
Ravi Chamakuri <rkc7h@virginia.edu>
2024-11-15 10:51:34 -0700

    Pushing header Fix

--- app/resource_requests/endpoints.py
+++ app/resource_requests/endpoints.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import g, request, make_response, jsonify
 from datetime import datetime
+from app import app
 from app.resource_requests.business import UVARCResourcRequestFormInfoDataManager
 
 
